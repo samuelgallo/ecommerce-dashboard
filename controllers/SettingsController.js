aa6edb8cd5ca926e0e21f4dd960abeb8760f6559
Samuel Gallo <samuel@samuelgallo.com>
2020-04-13 10:31:10 -0400

    prettier

--- controllers/SettingsController.js
+++ controllers/SettingsController.js
@@ -9,55 +9,55 @@ const fs = require('fs')
 const Settings = require('../models/Settings')
 
 exports.index = async (req, res) => {
-  try{
-    const data = await Settings.findOne()
-    res.render('settings', {title: 'Settings', store: data})
-  }catch(e) {
-    res.render('404')
-  }
+	try {
+		const data = await Settings.findOne()
+		res.render('settings', { title: 'Settings', store: data })
+	} catch (e) {
+		res.render('404')
+	}
 }
 
-exports.save = async (req, res) =>{
-  
-  try{
+exports.save = async (req, res) => {
+
+	try {
 
 
 
-    //console.log(req.file)
-    
-    const data = new Settings(req.body)
-    
-    const getFirstData = await Settings.findOne()
+		//console.log(req.file)
 
-    // const data = new Settings({
-    //   _id: getFirstData._id,
-    //   store_name: req.body.store_name,
-    //   hours: req.body.hours,
-    //   welcome: req.body.welcome,
-    // })
+		const data = new Settings(req.body)
 
-    if(req.file) {
-      
-      data.logo = await req.file
-    }else{
+		const getFirstData = await Settings.findOne()
 
-      data.logo = await getFirstData.logo
-    }
-    
- 
+		// const data = new Settings({
+		//   _id: getFirstData._id,
+		//   store_name: req.body.store_name,
+		//   hours: req.body.hours,
+		//   welcome: req.body.welcome,
+		// })
 
-    if(getFirstData) {
-      data._id = await getFirstData._id
+		if (req.file) {
 
-      Settings.updateOne(data).exec()
-    } else {
-      data.save()
-    }
+			data.logo = await req.file
+		} else {
 
-    res.redirect('/dashboard/settings')
+			data.logo = await getFirstData.logo
+		}
 
-  } catch(err) {
-    res.render('503')
-  }
+
+
+		if (getFirstData) {
+			data._id = await getFirstData._id
+
+			Settings.updateOne(data).exec()
+		} else {
+			data.save()
+		}
+
+		res.redirect('/dashboard/settings')
+
+	} catch (err) {
+		res.render('503')
+	}
 }
 
