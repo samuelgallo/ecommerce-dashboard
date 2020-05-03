f6138c4d76f7c2dd919e761ad3367f8dfde7915d
Samuel Gallo <samuel@samuelgallo.com>
2020-05-02 22:47:44 -0400

    fix routes

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const routes = express.Router()
 
-<<<<<<< HEAD
-=======
 // Just redirection to /dashboard
->>>>>>> master
 routes.get('/', (req, res) => {
   res.redirect('/dashboard')
 })
